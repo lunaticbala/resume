f2aba267917301e2b77b8e23ccbde79da0905a62
lunaticbala <87910861+lunaticbala@users.noreply.github.com>
2021-08-07 15:30:51 +0530

    Add files via upload

---
+++ resume data.js
@@ -0,0 +1,17 @@
+//JSON format resume data
+
+const data={
+    "name":"Balaji",
+    "age":"21",
+    "gender":"male",
+    "games":["kho-kho","badminton","soccer","cricket"],
+    "class":["HTML","JavaScript","CSS"],
+    "yop":"2021",
+    "location":"Neyveli"
+
+}
+
+console.log(data.name);
+console.log(data.class[0]);
+console.log(data.yop);
+console.log(data.location);
