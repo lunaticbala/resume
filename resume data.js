159458d79ea68cd9a8201983589e71d4e96f0e72
lunaticbala <87910861+lunaticbala@users.noreply.github.com>
2021-11-29 19:29:33 +0530

    Update resume data.js

--- resume data.js
+++ resume data.js
@@ -15,3 +15,44 @@ console.log(data.name);
 console.log(data.class[0]);
 console.log(data.yop);
 console.log(data.location);
+
+
+for loop
+
+var json = {
+    jsonData:  [
+        {one: [11, 12, 13, 14, 15]},
+        {two: [21, 22, 23]},
+        {three: [31, 32]}
+    ]
+ }; 
+ for (var i=0; i<json.jsonData.length; i++) {
+    for (var key in json.jsonData[i]) {
+        for (var j= 0; j<json.jsonData[i][key].length; j++) {
+            console.log(json.jsonData[i][key][j])
+        }
+    }
+ }
+
+
+ for in loop
+
+ var person = {
+    fname: "Nick",
+    lname: "Jonas",
+    age: 26
+ }; 
+ for (let x in person) {
+    console.log(x + ": "+ person[x])
+ }
+ ‍
+
+
+ for each
+
+
+ var results = [ {"id":"10", "class": "child-of-9"}, {"id":"11", "classd": "child-of-10"} ];
+
+results.forEach(function(item) {
+    console.log(item);
+});
